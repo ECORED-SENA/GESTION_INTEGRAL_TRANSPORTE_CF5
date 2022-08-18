5b6d7fcb06ff650b2dbb844a35bafb6653588dab
Va-Gomez <cmgomez72@misena.edu.co>
2022-08-18 00:03:16 -0500

    actualizando

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,22 +8,42 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    p.mb-4 A continuación, presentaremos el video que hará una breve introducción del presente componente formativo.
     
     figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
       figcaption Video: Leyenda del video
     
-    p.mb-5 La necesidad de mitigar la pandemia por COVID-19 ha obligado a la comunidad científica internacional, a desarrollar una serie de investigaciones y desarrollos tendientes a presentar una vacuna para hacerle frente a esta enfermedad que aumenta día a día su morbi mortalidad en Colombia. Es así, como en diciembre de 2020, la comunidad científica presenta los resultados de varios desarrollos de vacunas, que ofrecen eficacia y seguridad, respaldados por estudios clínicos rigurosos.
+    p.mb-5 En toda negociación hay dos personas con bienes o gustos diferentes que desean intercambiarlos, es decir hay dos actores que interactúan necesariamente: quien vende un producto y quien lo compra. Este intercambio se denomina mercado. Al proceso entre los suministros, producción, traslado al punto de destino, el ofrecimiento a uno o varios compradores y la adquisición del producto o servicio por un comprador se le denomina cadena de suministro, gestión logística o cadena logística. 
 
-    figure.mb-5
-      img(src="@/assets/curso/banner-intro.jpg", alt="Texto que describa la imagen")
-      figcaption Imagen 1. Vacunación
+    p.mb-5 Asimismo, en una cadena hay unos anillos o argollas, llamadas también ‘eslabones’, que son los elementos individuales que, articulados entre sí, forman la cadena que le confiere gran resistencia, fortaleza, pero también gran flexibilidad. Por lo tanto, podemos llamar cadena logística a la suma de eslabones que van desde la producción del bien o servicio hasta la adquisición del mismo por un usuario final o consumidor. 
 
-    p.mb-0 La llegada de nuevas vacunas al país supone un reto para el talento humano en salud responsable de la vacunación y desarrollar habilidades para ofrecer una orientación oportuna y segura a las personas objeto de la vacuna.
-      br
-      br
-      | En ese orden de ideas, este componente formativo guía al aprendiz hacia la identificación de los aspectos normativos, técnicos y operativos de la vacunación contra COVID19 en Colombia y de las generalidades de la infección por SARS-2 COVID 19.
+    .row
+      .col-md-4
+        figure
+          img(src='@/assets/curso/introduccion/img1.png', alt='Paquetes de cajas para enviar')
+      .col-md-8
+        .tarjeta.color-linea--borde.mb-0
+            p.mb-2 Basados en el anterior concepto, se incursionará en eslabones como: 
+            ul.lista-ul--color.mb-2
+              li 
+                i.fas.fa-angle-right
+                | Tipos de mercado. 
+              li 
+                i.fas.fa-angle-right
+                | Tipos de mercancías.
+              li 
+                i.fas.fa-angle-right
+                | Suministros de producción de bienes o servicios, denominados oferta.
+              li 
+                i.fas.fa-angle-right
+                | Y adquisición de estos bienes o servicios, lo que se denomina demanda de los mismos.
+
+            p Es aquí donde confluyen los factores de la demanda y la oferta de bienes y servicios. 
+
+               
 
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,12 +7,236 @@
       .titulo-principal__numero
         span 1
       h1  Oferta y Demanda
+
+    p.mb-4 Llamar leyes de la oferta y demanda es llamar leyes del mercado de bienes y servicios, que están dadas por las cambiantes condiciones que inciden en variables de venta y compra. Hay características que inciden en ellas, que incluyen su definición, características, ley de la oferta, ley de la demanda y factores que intervienen en dichas relaciones.
+
+    separador
     
     #t_1_1.titulo-segundo.color-acento-contenido
-      .h4 1.1 Infografía Animada
-    
+      .h4 1.1. Conceptos básicos
+
+    .row.mb-4
+      .col-md-5
+        .row.mb-4 
+          .col-md-2
+            figure
+              img(src='@/assets/curso/tema1/icon1.png', alt='Paquetes de cajas para enviar')
+          .col-md-10
+            .titulo-sexto.color-acento-contenido
+              h5 Figura 1
+              span - Pendiente positiva de oferta del mercado
+        p.mb-4 La oferta puede definirse como un número determinado de bienes o servicios con características de calidad y precio, que un productor o vendedor pone a disposición del público para ser adquiridos o consumidos. Al graficar las cantidades versus los precios de un determinado bien, se puede observar que se genera una curva de izquierda a derecha, que va incrementándose, lo cual significa que es una pendiente positiva. Esta relación entre cantidades y precios nos confirma que, a mejores precios del bien o servicio, la producción se incrementará.
+      .col-md-7
+        figure.mb-5
+          img(src='@/assets/curso/tema1/grafica1.png', alt='Gráfica explicativa')
+          figcaption Nota. Tomada de Enciclopedia Económica (2020).
+          
+    .row.mb-4
+      .col-md-5
+        .row.mb-4 
+          .col-md-2
+            figure
+              img(src='@/assets/curso/tema1/icon1.png', alt='Paquetes de cajas para enviar')
+          .col-md-10
+            .titulo-sexto.color-acento-contenido
+              h5 Figura 2
+              span - Pendiente negativa de la demanda del mercado
+        p.mb-4 Por su parte, la definición de demanda también está relacionada con el precio del bien y la cantidad del mismo. Al graficar estas dos variables, se observa que, al disminuir el precio de un bien o servicio, se aumenta la demanda del mismo y viceversa. Esta gráfica genera una curva descendente, que va de izquierda a derecha y se puede indicar que es una pendiente negativa.
+      .col-md-7
+        figure.mb-5
+          img(src='@/assets/curso/tema1/grafica2.png', alt='Gráfica explicativa')
+          figcaption Nota. Tomada de Enciclopedia Económica (2021).
+
+    .row.mb-4
+      .col-md-5
+        .row.mb-4 
+          .col-md-2
+            figure
+              img(src='@/assets/curso/tema1/icon1.png', alt='Paquetes de cajas para enviar')
+          .col-md-10
+            .titulo-sexto.color-acento-contenido
+              h5 Figura 3
+              span - Pendientes positivas y negativas de oferta y demanda
+        p.mb-4 La superposición de las dos gráficas, arroja un punto de intersección de las pendientes, al cual se le da el nombre de ‘punto de equilibrio’, siendo este el motivo de múltiples análisis para definir cuál es el precio ideal de venta de un producto según su cantidad y calidad. En este punto, tanto compradores como vendedores están de acuerdo en la cantidad que se compra o se vende, así como en el precio. Por encima del punto de equilibrio, los productores obtendrán ganancias o utilidades. Por debajo de éste, obtendrán pérdidas o muchos productos almacenados. (Enciclopedia Económica, 2020)
+      .col-md-7
+        figure.mb-5
+          img(src='@/assets/curso/tema1/grafica3.png', alt='Gráfica explicativa')
+          figcaption Nota. Tomada de Enciclopedia Económica (2021).
+    separador
+
     #t_1_2.titulo-segundo.color-acento-contenido
       .h4 1.2	Principios, pronósticos y métodos
+
+    p.mb-0 El mercado está regido por unas características, leyes y factores, los cuales permiten realizar estudios y análisis a fin de comprender sus alcances, pronósticos y métodos de análisis.
+    p.mb-4 A continuación, se presentan las características tanto de oferta como de demanda:
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-md-4
+        figure
+          img(src='@/assets/curso/tema1/img1.png', alt='Señal de bien con la mano')
+      .col-md-8
+        TabsC.color-acento-contenido
+          .py-3.py-md-4(titulo="Características de la oferta")
+            .row.p-2
+              .col-md-5
+                figure
+                  img(src='@/assets/curso/tema1/img2.png', alt='Hombre sonrriente')
+              .col-md-7.mb-4.mb-md-0
+                ul.lista-ul--color
+                  li 
+                    i.fas.fa-angle-right
+                    | Puede ser un bien o servicio.
+                  li 
+                    i.fas.fa-angle-right
+                    | Tiene una cantidad finita del bien que el vendedor pone a la venta.
+                  li 
+                    i.fas.fa-angle-right
+                    | La venta se realiza en un lapso de tiempo específico.
+                  li 
+                    i.fas.fa-angle-right
+                    | Dependerá del precio de venta.
+                  li 
+                    i.fas.fa-angle-right
+                    | Con el incremento de unidades en venta, se disminuyen los precios.
+
+          .py-3.py-md-4(titulo="Nidales")
+            .row.p-2
+              .col-md-5
+                figure
+                  img(src='@/assets/curso/tema1/img3.png', alt='Mujer sonrriendo')
+              .col-md-7.mb-4.mb-md-0
+                ul.lista-ul--color.mb-5
+                  li 
+                    i.fas.fa-angle-right
+                    | La demanda es analizada por el vendedor. 
+                  li 
+                    i.fas.fa-angle-right
+                    | El consumidor establece la demanda. 
+                  li 
+                    i.fas.fa-angle-right
+                    | Principal componente empresarial de fijación de precios.
+                  li 
+                    i.fas.fa-angle-right
+                    | Tienen gran injerencia las características socioeconómicas de la población.
+                  li 
+                    i.fas.fa-angle-right
+                    | Es directamente proporcional a la cantidad de productos o servicios.
+                  li 
+                    i.fas.fa-angle-right
+                    | El cambio en las relaciones de precios y cantidades demandadas se denomina ‘elasticidad’ y puede considerarse como demanda elástica, demanda inelástica o demanda normal.
+                .tarjeta.color-primario.p-3.mb-5
+                  .row.justify-content-around.align-items-center
+                    .col-4.col-sm-3.col-lg-2
+                      img(src="@/assets/componentes/ej-02.svg")
+                    .col
+                      .row.justify-content-between.align-items-center
+                        .col.mb-3.mb-sm-0
+                          h3.mb-1.text-white Figura 4
+                          p.text-small.text-white Tipos de demandas
+                        .col-auto
+                          a.boton.color-acento-botones.indicador__container(@click="modal1 = true")
+                            span Ver más
+                            .indicador--click(v-if="mostrarIndicador")
+
+                      ModalA(:abrir-modal.sync="modal1")
+                        .row.align-items-center
+                          .col-md-12.mb-4.mb-md-0
+                            .titulo-sexto.color-acento-contenido
+                              h5 Figura 4
+                              span - Tipos de demandas
+                            figure
+                              img(src='@/assets/curso/tema1/grafica4.png', alt='gráfica')
+                              figcaption Nota. Tomada de Moreno (2010).
+
+    p.mb-4 A continuación, se presentan las diferentes leyes y factores de la oferta y la demanda:
+
+    .row.mb-5
+      AcordionA(tipo="a" clase-tarjeta="tarjeta tarjeta-azul")
+        .row.align-items-center(titulo="Factores de la oferta y la demanda")
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1/img1.jpg', alt='Hombre escribiendo')
+
+          .col-md-7.mb-4.mb-md-0
+            h5.mb-4 Oferta:
+            p.mb-4 Siempre que todas las demás variables estén constantes, cuando se mejora el precio, se incrementan las cantidades o se producen más bienes.
+            h5.mb-4 Demanda:
+            p.mb-4 Siempre que todas las demás variables estén constantes, es inversamente proporcional entre el aumento del precio y la disminución de la cantidad demandada y, de la misma manera, inversamente proporcional entre la disminución del precio y el aumento de la cantidad demandada.
+
+
+        .row.align-items-center(titulo="Leyes de la oferta y la demanda")
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1/img2.jpg', alt='gráfica')
+
+          .col-md-7.mb-4.mb-md-0
+            p.mb-4 La oferta y la demanda de bienes o servicios contemplan unos factores o condiciones que se comparan en la siguiente matriz:
+            
+    .titulo-principal1.color-acento-contenido.mb-5
+      .titulo-principal1__numero
+        span 
+      h3.mb-0 Principios de la Oferta y la Demanda
+
+    p.mb-4 A partir de las leyes de oferta y demanda, se conciben ocho principios que se describen como cambios en el equilibrio del mercado, de la siguiente manera:
+
+    .row.mb-0
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+      
+    .row.mb-4
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
+      .col-md-3
+        .bloque-texto-a.color-secundario.p-4.p-md-5.mb-3
+          .row.m-0.align-items-center.justify-content-between
+            .col-lg-4.mb-4.mb-lg-0
+              h2.mb-0 
+            .col-lg-8
+
 
     #t_1_3.titulo-segundo.color-acento-contenido
       .h4 1.3	Actividades económicas
@@ -23,14 +247,10 @@
 </template>
 
 <script>
-import Muestras from '../components/Muestras' // borrar una vez el componente "Muestras" no se necesite
 export default {
   name: 'Tema1',
-  components: {
-    Muestras, // borrar una vez el componente "Muestras" no se necesite
-  },
   data: () => ({
-    // variables de vue
+    modal1: false,
   }),
   mounted() {
     this.$nextTick(() => {
